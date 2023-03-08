c2a38d1e1e4c91c10e905964318beb6f2feed1e4
reistence <gvlamberti@gmail.com>
2023-03-08 16:14:40 +0100

    more efficient fizzbuzz implemented

--- src/org/lessons/java/FizzBuzz.java
+++ src/org/lessons/java/FizzBuzz.java
@@ -11,23 +11,32 @@ public class FizzBuzz {
         do {
         System.out.print("How many nr do you want? ");
          userRange = Integer.parseInt(scanner.nextLine());
-
         } while (userRange == 0 || userRange > 1000);
 
-            for (int i = 1; i < userRange + 1; i++) {
-                String output = "";
+      /*  for (int i = 1; i < userRange + 1; i++) {
                 if (i % 3 == 0 && i % 5 == 0) {
-                    System.out.println(output + "FizzBuzz");
+                    System.out.println("FizzBuzz");
                 } else if (i % 3 == 0) {
-                    System.out.println(output + "Fizz");
+                    System.out.println("Fizz");
                 } else if (i % 5 == 0) {
-                    System.out.println(output + "Buzz");
+                    System.out.println("Buzz");
                 } else {
                     System.out.println(i);
                 }
 
+        }*/
+        for (int i = 1; i < userRange + 1; i++){
+            String output = "";
+            if (i % 3 == 0){
+              output += "Fizz";
+            }
+            if (i % 5 == 0){
+               output += "Buzz";
+            }
+            System.out.println(output.length() > 0 ? output : i);
 
         }
+
 
     }
 
