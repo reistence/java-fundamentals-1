303dc95897070aae713ecdb8ac5ecdf0b0b6504a
reistence <gvlamberti@gmail.com>
2023-03-08 15:23:22 +0100

    exes solved

--- src/org/lessons/java/FizzBuzz.java
+++ src/org/lessons/java/FizzBuzz.java
@@ -1,4 +1,34 @@
 package org.lessons.java;
+import java.util.Scanner;
+
 
 public class FizzBuzz {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int userRange;
+        do {
+        System.out.print("How many nr do you want? ");
+         userRange = Integer.parseInt(scanner.nextLine());
+
+        } while (userRange == 0 || userRange > 1000);
+
+            for (int i = 1; i < userRange + 1; i++) {
+                String output = "";
+                if (i % 3 == 0 && i % 5 == 0) {
+                    System.out.println(output + "FizzBuzz");
+                } else if (i % 3 == 0) {
+                    System.out.println(output + "Fizz");
+                } else if (i % 5 == 0) {
+                    System.out.println(output + "Buzz");
+                } else {
+                    System.out.println(i);
+                }
+
+
+        }
+
+    }
+
 }
--- src/org/lessons/java/TrainTicket.java
+++ src/org/lessons/java/TrainTicket.java
@@ -1,7 +1,38 @@
 package org.lessons.java;
-
+import java.text.DecimalFormat;
+import java.util.Scanner;
 public class TrainTicket {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // get inputs and ini scanner
+        Scanner scanner = new Scanner(System.in);
+        int userAge;
+        int distance;
+        System.out.print("What is your age? ");
+        userAge = Integer.parseInt(scanner.nextLine());
+        System.out.print("How far would you travel?(km) ");
+        distance = Integer.parseInt(scanner.nextLine());
+
+        double price;
+        double discount;
+        double total;
+
+
+        if (userAge <= 18){
+            price = (double) distance * 0.21;
+            discount = (price * 20 ) / 100;
+        } else if (userAge >= 65){
+            price = (double) distance * 0.21;
+            discount = (price * 40 ) / 100;
+        } else {
+            price = (double) distance * 0.21;
+            discount = 0;
+        }
+
+        total = price - discount;
+
+        String formattedTotal = new DecimalFormat("#.##").format(total);
+        System.out.println("€ " +formattedTotal);
+
+
     }
 }
